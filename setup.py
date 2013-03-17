0a2672e41989a6349c43809fd101956903d6a7aa
Javier Santacruz <javier.santacruz.lc@gmail.com>
2013-03-17 19:43:02 +0100

    Adds package information

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mp3hash',
+    version='0.0.1',
+    description='Music file hasher',
+    author='Javier Santacruz',
+    author_email='javier.santacruz.lc@gmail.com',
+    url='http://github.com/jvrsantacruz/mp3hash',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    platforms=['Any'],
+    scripts=['mp3hash.py'],
+)
