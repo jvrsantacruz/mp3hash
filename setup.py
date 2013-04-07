b151578bee89f4f33724366b1f71410e211d4353
Javier Santacruz <javier.santacruz.lc@gmail.com>
2013-04-07 20:28:19 +0200

    Increases version number to 0.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readme = os.path.join(here, 'README.rst')
 
 setup(
     name='mp3hash',
-    version='0.0.4',
+    version='0.0.5',
     description='Music file hasher',
     long_description=open(readme).read(),
     author='Javier Santacruz',
