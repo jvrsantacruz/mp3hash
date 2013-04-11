8971cd7310da95e598299fdd1c8ada84e7e43309
Javier Santacruz <javier.santacruz.lc@gmail.com>
2013-04-12 01:10:24 +0200

    Adds cli integration tests and doublex to dev-reqs

---
+++ tests/integration/test_cli.py
@@ -0,0 +1,81 @@
+#-*- coding: utf-8 -*-
+
+import sys
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from io import StringIO  # python3
+
+
+from doublex import Spy, called
+from hamcrest import assert_that, contains_string
+
+
+from cli import main
+
+
+class TestCLI(object):
+    def setup(self):
+        # monkeypatchs sys
+        # I know, I know...
+        self.old_stdout = sys.stdout
+        self.old_stderr = sys.stderr
+
+        self.stdout = sys.stdout = StringIO()
+        self.stderr = sys.stderr = StringIO()
+
+        self.exit = Spy().exit
+        self.old_sys_exit = sys.exit
+        sys.exit = self.exit
+
+        self.argv = [sys.argv[0]]
+        self.old_sys_argv = sys.argv
+        sys.argv = self.argv
+
+    def teardown(self):
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
+        sys.exit = self.old_sys_exit
+        sys.argv = self.old_sys_argv
+
+    def test_main_with_no_arguments_stdouts_error(self):
+        main()
+
+        assert_that(self.stdout.getvalue(),
+                    contains_string(u'Insufficient arguments'))
+
+    def test_main_with_no_arguments_exits_with_code_2(self):
+        main()
+
+        assert_that(self.exit, called().with_args(2))
+
+    def test_main_with_negative_maxbytes_value_stdouts_error(self):
+        self.argv.extend(['--maxbytes', '-5'])
+
+        main()
+
+        assert_that(self.stdout.getvalue(),
+                    contains_string(u'Invalid value for --maxbytes'))
+
+    def test_main_with_negative_maxbytes_value_exits_with_code_1(self):
+        self.argv.extend(['--maxbytes', '-5'])
+
+        main()
+
+        assert_that(self.exit, called().with_args(1))
+
+    def test_main_with_0_maxbytes_value_stdouts_error(self):
+        self.argv.extend(['--maxbytes', '0'])
+
+        main()
+
+        assert_that(self.stdout.getvalue(),
+                    contains_string(u'Invalid value for --maxbytes'))
+
+    def test_main_with_0_maxbytes_value_exits_with_code_1(self):
+        self.argv.extend(['--maxbytes', '0'])
+
+        main()
+
+        assert_that(self.exit, called().with_args(1))
