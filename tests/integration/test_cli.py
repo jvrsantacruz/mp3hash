4740000ee6e51a78d6e5957493f37027bdbe9aa7
Javier Santacruz <javier.santacruz.lc@gmail.com>
2013-04-14 18:34:08 +0200

    Adds further cli integration tests

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -1,17 +1,21 @@
 #-*- coding: utf-8 -*-
 
+import os
 import errno
+import hashlib
 import subprocess
 
-from hamcrest import assert_that, contains_string, is_, all_of
-
+from hamcrest import *
 from tests.integration import SONG1_PATH, SONG2_PATH
+
+import mp3hash
 
 
 OK = 0
 SCRIPT = 'mp3hash'
 NON_EXISTENT_PATH = '/non/existent/path'
 NON_EXISTENT_ALGORITHM = 'I am not a hash'
+NON_EXISTENT_FILE = 'nonexistent.txt'
 
 
 def call(*args):
@@ -21,7 +25,7 @@ def call(*args):
         return command.returncode, command.output
 
 
-class TestCLI(object):
+class TestParameterValidation(object):
     def test_no_arguments_stdouts_error(self):
         retcode, output = call(SCRIPT)
 
@@ -31,6 +35,33 @@ class TestCLI(object):
         retcode, output = call(SCRIPT)
 
         assert_that(retcode, is_(errno.EINVAL))
+
+    def test_non_existent_path_returns_ok(self):
+        retcode, output = call(SCRIPT, NON_EXISTENT_PATH)
+
+        assert_that(retcode, is_(OK))
+
+    def test_non_existent_path_outputs_error(self):
+        retcode, output = call(SCRIPT, NON_EXISTENT_PATH)
+
+        assert_that(output, contains_string('does not exist'))
+
+
+class TestListAlgorithmsOption(object):
+    def test_list_algorithms_returns_ok(self):
+        retcode, output = call(SCRIPT, '--list-algorithms')
+
+        assert_that(retcode, is_(OK))
+
+
+class TestMaxbytesOption(object):
+    def test_maxbytes_option_changes_maxbytes_used(self):
+        maxbytes = 1000
+        hash = mp3hash.mp3hash(SONG1_PATH, maxbytes=maxbytes)
+
+        retcode, output = call(SCRIPT, SONG1_PATH, '--maxbytes', str(maxbytes))
+
+        assert_that(output, starts_with(hash))
 
     def test_negative_maxbytes_value_stdouts_error(self):
         retcode, output = call(SCRIPT, '--maxbytes', '-5', '*')
@@ -52,20 +83,22 @@ class TestCLI(object):
 
         assert_that(retcode, is_(errno.EINVAL))
 
-    def test_list_algorithms_returns_ok(self):
-        retcode, output = call(SCRIPT, '--list-algorithms')
 
-        assert_that(retcode, is_(OK))
+class TestHashOption(object):
+    def test_existent_file_outputs_hash_only_if_hash_opt_is_present(self):
+        hash = mp3hash.mp3hash(SONG1_PATH)
 
-    def test_non_existent_path_returns_ok(self):
-        retcode, output = call(SCRIPT, NON_EXISTENT_PATH)
+        retcode, output = call(SCRIPT, SONG1_PATH, '--hash')
 
-        assert_that(retcode, is_(OK))
+        assert_that(output, is_(hash + '\n'))
 
-    def test_non_existent_path_outputs_error(self):
-        retcode, output = call(SCRIPT, NON_EXISTENT_PATH)
 
-        assert_that(output, contains_string('does not exist'))
+class TestAlgorithmOption(object):
+    def test_non_existent_algorithm_outputs_one_line_error(self):
+        retcode, output = call(
+            SCRIPT, '--algorithm', NON_EXISTENT_ALGORITHM, '*')
+
+        assert_that(output.count('\n'), is_(1))
 
     def test_non_existent_algorithm_outputs_error(self):
         retcode, output = call(
@@ -76,18 +109,77 @@ class TestCLI(object):
             contains_string('algorithm')
         ))
 
-    def test_non_existent_algorithm_outputs_one_line_error(self):
-        retcode, output = call(
-            SCRIPT, '--algorithm', NON_EXISTENT_ALGORITHM, '*')
+    def test_default_algorithm_is_sha1(self):
+        hasher = hashlib.new('sha1')
+        hash = mp3hash.mp3hash(SONG1_PATH, hasher=hasher)
 
-        assert_that(output.count('\n'), is_(1))
+        retcode, output = call(SCRIPT, SONG1_PATH)
 
+        assert_that(output, starts_with(hash))
+
+    def test_algorithm_option_changes_algorithm_used(self):
+        algorithm = 'md5'
+        hasher = hashlib.new(algorithm)
+        hash = mp3hash.mp3hash(SONG1_PATH, hasher=hasher)
+
+        retcode, output = call(SCRIPT, SONG1_PATH, '--algorithm', algorithm)
+
+        assert_that(output, starts_with(hash))
+
+
+class TestOutputOption(object):
+    def setup(self):
+        try:
+            os.unlink(NON_EXISTENT_FILE)
+        except OSError:
+            pass
+
+    def test_output_changes_output_and_stdouts_nothing(self):
+        retcode, output = call(SCRIPT, SONG1_PATH,
+                               '--output', NON_EXISTENT_FILE)
+
+        assert_that(output, is_(u''))
+
+    def test_output_redirects_output_to_file_in_given_path(self):
+        hash = mp3hash.mp3hash(SONG1_PATH)
+        filename = os.path.basename(SONG1_PATH)
+
+        retcode, output = call(SCRIPT, SONG1_PATH,
+                               '--output', NON_EXISTENT_FILE)
+
+        with open(NON_EXISTENT_FILE) as output_file:
+            assert_that(output_file.read(), is_(hash + u' ' + filename + '\n'))
+
+
+class TestHashing(object):
     def test_existent_file_returns_ok(self):
         retcode, output = call(SCRIPT, SONG1_PATH)
 
         assert_that(retcode, is_(OK))
 
+    def test_existent_file_outputs_one_single_line(self):
+        retcode, output = call(SCRIPT, SONG1_PATH)
+
+        assert_that(output.count('\n'), is_(1))
+
+    def test_existent_file_outputs_hash_space_filename(self):
+        hash = mp3hash.mp3hash(SONG1_PATH)
+        filename = os.path.basename(SONG1_PATH)
+
+        retcode, output = call(SCRIPT, SONG1_PATH)
+
+        assert_that(output, is_(hash + u' ' + filename + '\n'))
+
     def test_several_existent_files_returns_ok(self):
-        retcode, output = call(SCRIPT, SONG1_PATH, SONG2_PATH)
+        paths = [SONG1_PATH, SONG2_PATH]
+
+        retcode, output = call(SCRIPT, *paths)
 
         assert_that(retcode, is_(OK))
+
+    def test_several_existent_files_outputs_several_lines(self):
+        paths = [SONG1_PATH, SONG2_PATH]
+
+        retcode, output = call(SCRIPT, *paths)
+
+        assert_that(output.count('\n'), is_(len(paths)))
